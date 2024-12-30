ce13c73615855b46fa0c7288c2e0e462f6a7f33d
farazalam2017 <farazalam2017@gmail.com>
2024-12-30 11:19:13 +0530

    minor updates

--- Utilities/assets/js/script.js
+++ Utilities/assets/js/script.js
@@ -129,3 +129,18 @@ document.addEventListener("DOMContentLoaded", function () {
     checkEmail();
   }
 });
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('.main-nav-links').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+  });
+});
